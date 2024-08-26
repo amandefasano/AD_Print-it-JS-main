32007c5abd2e512a34922a84ae2fce84cbd12efa
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-08-26 14:52:55 +0300

    Refactor: silder's image change on click on an arrow

--- assets/script.js
+++ assets/script.js
@@ -19,31 +19,12 @@ const slides = [
   },
 ];
 let img_slider = document.querySelector(".banner-img");
-let src_value = img_slider.getAttribute("src");
-let slide_src_value = getSlideSrcValue(src_value);
+let img = slides[0].image;
+let slide_img = getSlideSrcValue(img);
 
-function getSlideSrcValue(src_value) {
-  let split_value = src_value.split("/");
-  let slide_src_value = split_value[4].split(".");
-  return slide_src_value[0];
-}
-
-function increaseSlideSrcValue(slide_src_value) {
-  let slide_src_value_index = slide_src_value.charAt(5);
-  slide_src_value_index = parseInt(slide_src_value_index);
-  slide_src_value_index++;
-  slide_src_value = slide_src_value.slice(0, 5);
-  let new_slide_src_value = `${slide_src_value}` + `${slide_src_value_index}`;
-  return new_slide_src_value;
-}
-
-function decreaseSlideSrcValue(slide_src_value) {
-  let slide_src_value_index = slide_src_value.charAt(5);
-  slide_src_value_index = parseInt(slide_src_value_index);
-  slide_src_value_index--;
-  slide_src_value = slide_src_value.slice(0, 5);
-  let new_slide_src_value = `${slide_src_value}` + `${slide_src_value_index}`;
-  return new_slide_src_value;
+function getSlideSrcValue(img) {
+  let slide_img = img.split(".");
+  return slide_img[0];
 }
 
 // Slider's dots
@@ -63,41 +44,36 @@ for (let i = 0; i < slides.length; i++) {
 }
 
 /* Selected dot */
-
 // Slider's arrows
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
+let j = 0;
+dots[j].classList.add("dot_selected");
 
 arrow_left.addEventListener("click", (event) => {
   event.preventDefault();
-  slide_src_value = decreaseSlideSrcValue(slide_src_value);
-  for (let j = dots.length -1; j >= 0 ; j--) {
-    if (dots[j].id === slide_src_value) {
-      console.log(
-        `dots[j].id = ${dots[j].id} ET slide_src_value = ${slide_src_value}`
-      );
-      dots[j+1].classList.remove("dot_selected");
-      dots[j].classList.toggle("dot_selected");
-    }
+  j--;
+  slide_img = slides[j].image;
+  let slide_img_id = getSlideSrcValue(slide_img);
+  if (dots[j].id === slide_img_id) {
+    dots[j + 1].classList.remove("dot_selected");
+    dots[j].classList.toggle("dot_selected");
   }
-  let sliced_src_value = src_value.slice(0, 26)
-  src_value = `${sliced_src_value}` + `${slide_src_value}` + `.jpg`
-  img_slider.src = src_value
+  let sliced_img = img_slider.src.slice(0, 46);
+  img = sliced_img + slide_img;
+  img_slider.src = img;
 });
 
 arrow_right.addEventListener("click", (event) => {
   event.preventDefault();
-  slide_src_value = increaseSlideSrcValue(slide_src_value);
-  for (let j = 0; j < dots.length; j++) {
-    if (dots[j].id === slide_src_value) {
-      console.log(
-        `dots[j].id = ${dots[j].id} ET slide_src_value = ${slide_src_value}`
-      );
-      dots[j-1].classList.remove("dot_selected");
-      dots[j].classList.toggle("dot_selected");
-    }
+  j++;
+  slide_img = slides[j].image;
+  let slide_img_id = getSlideSrcValue(slide_img);
+  if (dots[j].id === slide_img_id) {
+    dots[j - 1].classList.remove("dot_selected");
+    dots[j].classList.toggle("dot_selected");
   }
-  let sliced_src_value = src_value.slice(0, 26)
-  src_value = `${sliced_src_value}` + `${slide_src_value}` + `.jpg`
-  img_slider.src = src_value
+  let sliced_img = img_slider.src.slice(0, 46);
+  img = sliced_img + slide_img;
+  img_slider.src = img;
 });
